2c6a7663b2efd15bc871c9d8a66ac36384966cba
tue227 <tue227@c211fff6-4677-46eb-afd0-305d7f7c76a1>
2015-06-08 12:22:56 +0000

    update config for backend findnex

---
+++ backend/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=findnex',
+    'username' => 'root',
+    'password' => 'admin123',
+    'charset' => 'utf8',
+];
--- backend/config/params.php
+++ backend/config/params.php
@@ -1,4 +1,6 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'static_url' => 'http://'.$_SERVER['SERVER_NAME'].'/themes/',
+    'url_upload' => 'http://'.$_SERVER['SERVER_NAME'].'/upload/'
 ];
